36c46fc0083f420e1754a6302ba35ac71a4af482
git <laghomklaus@yahoo.fr>
2025-08-27 14:58:11 -0700

    first final commit for review2

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/newsExplorer/",
+  // base: "/newsExplorer/",
   plugins: [react()],
-  // server: {
-  //   host: "0.0.0.0",
-  //   port: 3000,
-  // },
+  server: {
+    port: 3000,
+  },
 });
